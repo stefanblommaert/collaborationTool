8a3ca3316c13fabc4ea1598ad1012751689aa7a9
Stefan <Stefan@MacBook-Pro-van-Stefan.local>
2016-10-21 15:10:32 +0200

    Pull request fix of the web application

--- server.js
+++ server.js
@@ -1,24 +1,20 @@
 var express = require('express'),
 	stylus = require('stylus'),
 	logger = require('morgan'),
-	bodyParser = require('body-parser'),
-	mongoose = require('mongoose');
-
-var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development'; 
-
+	bodyparser = require('body-parser'),
+	mongoose = require('mongoose'),
+	request = require("request");
 var app = express();
- 
-function compile(str, path){
-	return stylus(str).set('filename', path);
-}
 
-app.set('views', __dirname + '/server/views');
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'ejs');
+app.use(bodyparser.json());
 
+app.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin","*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-requested-With, Content-Type, Accept");
+	next();
+});
 
-app.use(express.static('server/views'));
-
+//MongoDB
 mongoose.Promise = global.Promise; //Geeft de error 'mongoose mpromise is deprecated' niet meer
 mongoose.connect('mongodb://localhost/tool'); //connectie met de DB genaamd 'tool'
 var db = mongoose.connection;
@@ -34,11 +30,52 @@ var mongoMessage = Message.findOne().exec(function(err, messageDoc) {
 	mongoMessage = messageDoc.message;
 });
 
-app.get('/', function(req, res){
-	res.render('navbar.html', { //algemene html pagina ophalen om in te routeren
-		mongoMessage: mongoMessage
-	}); 
-})
+
+var path = require("path");
+app.use(express.static('server/views'));
+
+app.get("/", function(req,res){
+	res.sendFile( __dirname + "/server/views/" + "navbar.html");
+	mongoMessage: mongoMessage
+});
+
+
+var rooms = [
+	{ klas : 'test1', leraar : 'test1', tittel : 'test1', code : 'test1'},
+	{ klas : 'test2', leraar : 'test2', tittel : 'test2', code : 'test2'}
+];
+
+app.get("/getRooms",function(req,res) {
+	res.json(rooms);
+	console.log('rooms werden gestuurd');
+	});
+
+app.post("/form",function(req,res){ 
+	if (req.body.klas == "" || req.body.leraar == "" || req.body.tittel == "" || req.body.code == "") {
+		res.statusCode = 400;
+		return res.send('error 400: post syntax incorrect');
+	} 
+		
+	var newRoom = {
+	klas : req.body.klas,
+	leraar : req.body.leraar,
+	tittel : req.body.tittel,
+	code : req.body.code
+	};
+
+	rooms.push(newRoom);
+	console.log("received booking");
+	console.log(rooms);
+	res.json(true);
+
+	var stringRooms = JSON.stringify(rooms);
+
+	fs = require('fs');
+	fs.writeFile('rooms.txt', stringRooms, function (err) {
+  	if (err) return console.log(err);
+  	console.log('rooms werd opgeslagen');
+	});
+});
 
 
 var port = 3000;
